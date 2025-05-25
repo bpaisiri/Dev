bd49782b78b9354402bf8a6a3945f2683acc5703
aisiri_bp <aisiribp2004@gmail.com>
2025-05-26 00:56:11 +0530

    Update test2.java

--- test2.java
+++ test2.java
@@ -8,3 +8,72 @@ class test2 {
         System.out.println("The sum of " + num1 + ", " + num2 + ", and " + num3 + " is: " + sum);
     }
 }
+
+
+
+FROM openjdk:23
+WORKDIR /app
+COPY test.java .
+RUN javac test.java
+CMD ["java","test"]
+
+FROM python:3.12
+WORKDIR /app
+COPY test1.py Users/Palchander/Desktop/test/test1.py
+CMD ["python","Users/Palchander/Desktop/test/test1.py"]
+
+
+package vvce.nitk;
+
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+public class demo1 {
+
+		public static void main(String[] args) throws IOException, ParseException {
+			JSONParser jsonparser = new JSONParser();
+			FileReader reader = new FileReader(".\\JSON\\demo.json");
+			Object obj = jsonparser.parse(reader);
+			JSONObject empjsonobj = (JSONObject)obj;
+			
+			String fname = (String) empjsonobj.get("firstname");
+			String lname = (String) empjsonobj.get("lastname");
+			
+			System.out.println("first name:"+fname);
+			System.out.println("Last name:"+lname);
+			
+		}
+}
+
+
+modification
+
+package vvce.testing;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class aisi {
+	    public static void main(String[] args) {
+	        Properties props = new Properties();
+	        try {
+	            // Load the properties file
+	            FileInputStream fis = new FileInputStream("JDBC/abc.properties");
+	            props.load(fis);
+
+	            // Read the values
+	            String firstName = props.getProperty("firstName");
+	            String lastName = props.getProperty("lastName");
+
+	            // Print output
+	            System.out.println("First Name: " + firstName);
+	            System.out.println("Last Name: " + lastName);
+
+	            fis.close();
+	        } catch (IOException e) {
+	            System.out.println("Error reading properties file: " + e.getMessage());
+	        }
+	    }
+	}
